47c1f4f723183f5965983ee84334957b50a9a118
KMJ_DEV <dds13462@gmail.com>
2022-12-30 18:13:36 +0900

    slider01 좌우버튼

--- 04/slide01.js
+++ 04/slide01.js
@@ -5,20 +5,22 @@ $(function () {
   const button = $("#buttonList>li");
   let current = 0; //현재보고있는 놈
   let id; //setIntervalId =>자동
+
+  let btnIdx = 0; //클릭한 페이저 버튼의 인덱스
   const speed = 3000;
 
   //버튼클릭함수
   button.click(function () {
     //$(this) => 제이쿼리 객체화
     //index() 인덱스번호 받아오기
-    let btnIdx = $(this).index();
+    btnIdx = $(this).index();
     //console.log(btnIdx);
 
     //선택된 것만 적용하기
     //1. 다 지우고 걔만 적용
     button.removeClass("on");
     $(this).addClass("on");
-    move(btnIdx);
+    move();
   });
 
   //시간마다 실행할 함수
@@ -31,15 +33,14 @@ $(function () {
       if (next == visual.length) {
         next = 0;
       }
-      console.log(next);
+      //console.log(next);
       button.eq(next).trigger("click");
-
       //console.log(current++);
     }, speed);
   }
 
   //슬라이드 이동시키는 함수
-  function move(btnIdx) {
+  function move() {
     if (current == btnIdx) {
       return;
       //밑에꺼 실행시키지 않는다.
@@ -56,12 +57,55 @@ $(function () {
   }
 
   /* 자동재생 멈춤 */
-  $("#brandVisual,#buttonList").mouseenter(function () {
-    clearInterval(id);
-  });
-  $("#brandVisual ,#buttonList").mouseleave(function () {
-    timer();
-  });
+  clearAuto();
+  function clearAuto() {
+    $("#brandVisual,#buttonList, .controls").mouseenter(function () {
+      clearInterval(id);
+    });
+    $("#brandVisual ,#buttonList, .controls").mouseleave(function () {
+      timer();
+    });
+  }
+
+  /* 좌우 컨트롤 버튼 */
+  controls();
+  function controls(){
+    $('.controls .next').click(function(){
+      btnIdx =btnIdx+1;
+
+      if(btnIdx == visual.length){
+        btnIdx=0;
+      }
+      button.removeClass("on");
+      button.eq(btnIdx).addClass('on');
+
+      let cu = visual.eq(current);
+      let ne = visual.eq(btnIdx);
+      cu.css("left","0").stop().animate({left:"-100%"})
+      ne.css("left","100%").stop().animate({left:"0%"})
+      current=btnIdx;
+    })
+
+    $('.controls .prev').click(function(){
+      btnIdx =btnIdx-1;
+
+      //console.log(current);
+      if(current == 0){
+        btnIdx=visual.length-1;
+      }
+      button.removeClass("on");
+      button.eq(btnIdx).addClass('on');
+
+      let cu = visual.eq(current);
+      let pr = visual.eq(btnIdx);
+      cu.css("left","0").stop().animate({left:"100%"})
+      pr.css("left","-100%").stop().animate({left:"0%"})
+      current=btnIdx;
+    })
+
+  }
+
+
 
 }); //jQuery
 
