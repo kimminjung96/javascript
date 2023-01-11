f3f48697852cec61f9adb5807806b120039b8c6a
KMJ_DEV <dds13462@gmail.com>
2023-01-11 16:40:32 +0900

    스크롤양 수정

--- 06/06/08jq.js
+++ 06/06/08jq.js
@@ -8,10 +8,11 @@ $(window).scroll(function () {
   scrollTop = $(this).scrollTop();
   let scrollHeight = section.height();
   let scrollRealHeight = scrollHeight - windowHeight;
+  let scrollPercent = Math.floor((scrollTop / scrollRealHeight) * 100);
+  if (scrollPercent >= 100) {
+    scrollPercent = 100;
+  }
 
-  let scrollPercent = (scrollTop / scrollRealHeight) * 100;
-
-  text.text(Math.floor(scrollPercent) + "%");
-  progressBar.css("width",scrollPercent + "%")
-  
+  text.text(scrollPercent + "%");
+  progressBar.css("width", scrollPercent + "%");
 });
