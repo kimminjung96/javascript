0661bb95c6c42d62c3ee7e537f6a6420d5c1cb96
KMJ_DEV <dds13462@gmail.com>
2023-03-02 11:49:37 +0900

    gsap

--- modules/say.js
+++ modules/say.js
@@ -1,3 +1,11 @@
-function say(user){
-alert(`안녕하세요,${user}님`)
+/* 
+export function say(user) {
+  alert(`안녕하세요,${user}님`);
 }
+ */
+
+const app =(user)=>{
+    return alert(`안녕하세요,${user}님`);
+}
+
+export default app();
