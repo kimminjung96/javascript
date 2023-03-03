d3263f9e0c082463826843b3e25990c0af3cb3a1
KMJ_DEV <dds13462@gmail.com>
2023-03-03 09:39:33 +0900

    module

--- modules/say.js
+++ modules/say.js
@@ -1,11 +1,11 @@
-/* 
+/*
 export function say(user) {
-  alert(`안녕하세요,${user}님`);
+	alert(`안녕하세요,${user}님`);
 }
- */
+*/
+const app = (user) => {
+  return alert(`안녕하세요,${user}님`);
+};
 
-const app =(user)=>{
-    return alert(`안녕하세요,${user}님`);
-}
-
-export default app();
+//개체 한개만 있는경우
+export default app;
